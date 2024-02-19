610ffe1b07f5ce7f311025a89df31cd9d1932116
marouaneTalbi <talbi.918marouane@gmail.com>
2024-02-19 22:06:49 +0100

    first commit

---
+++ sql.sql
@@ -0,0 +1,16 @@
+CREATE TYPE card_category AS ENUM ('FIRST', 'SECOND', 'THIRD', 'FOURTH', 'FIFTH', 'SIXTH', 'SEVENTH', 'DONE');
+
+CREATE TABLE "Cards" (
+    "id" SERIAL PRIMARY KEY,
+    "question" VARCHAR(255),
+    "answer" VARCHAR(255),
+    "tag" VARCHAR(255),
+    "category" card_category DEFAULT 'FIRST'
+);
+
+CREATE TABLE "Learning" (
+    "id" SERIAL PRIMARY KEY,
+    "isValid" BOOLEAN DEFAULT false,
+    "date" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    "cardId" INTEGER REFERENCES "Cards"("id")
+);
