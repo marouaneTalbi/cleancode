34017922c881a1afceccbd011ac2f4f619a292f2
= <=>
2024-02-20 16:56:15 +0100

    feat(quizz): add input to answer at question #wip

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 'use client';
 import './App.css';
-import { useState, useEffect } from 'react';
-import { Button, Modal, Label, TextInput } from 'flowbite-react';
+import { useState } from 'react';
+import { Button, Modal, Label, TextInput, Card } from 'flowbite-react';
 import axios from 'axios';
 
 function App() {
 
   const [openModal, setOpenModal] = useState(false);
   const [cards, setCards] = useState([]);
+  const [startQuiz, setStartQuiz] = useState(false);
+  const [cardResponses, setCardResponses] = useState({});
   const [formData, setFormData] = useState({
     question: '',
     answer: '',
@@ -34,10 +36,15 @@ function App() {
   const createCard = async () => {
     try {
       const response = await axios.post('http://localhost:4000/cards', formData);
-      setCards([...cards, response.data]);
+      //setCards([...cards, response.data]);
     } catch (error) {
       console.error('Erreur lors de la création de la carte mémoire :', error);
     }
+  }
+
+  const handleQuiz = () => {
+    getCards();
+    setStartQuiz(true);
   }
 
   const handleSubmit = (event) => {
@@ -52,11 +59,46 @@ function App() {
     setOpenModal(false);
   };
 
+  const handleAnswer = (event) => {
+    
+  }
+
+  const handleChangeAnswer = (event, question) => {
+    event.preventDefault();
+    const { value } = event.target;
+    setCardResponses((prevResponses) => ({
+      ...prevResponses,
+      [question]: value
+    }));
+  }; 
+  
   return (
     <div className="p-4">
       <div className='flex flex-row gap-6'>
-        <Button color='purple' onClick={() => setOpenModal(true)}>Créer une carte mémoire</Button>
-        <Button color='gray'>Commener un questionnaire</Button>
+        <Button onClick={() => setOpenModal(true)}>Créer une carte mémoire</Button>
+        <Button color='gray' onClick={() => handleQuiz()}>Commencer un questionnaire</Button>
+      </div>
+      <div className='flex flex-row gap-6 mt-6'>
+        {
+          startQuiz && cards.map((card, index) => (
+            <Card href="#" className="max-w-sm">
+              <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                {card.question}
+              </h5>
+              <form onSubmit={handleAnswer}>
+                <TextInput
+                  id={`answer-${card.question}`}
+                  type="text"
+                  placeholder="Votre réponse"
+                  value={cardResponses[card.question] || ''}
+                  onChange={(event) => handleChangeAnswer(event, card.question)}
+                  required
+                />
+                <Button type='submit'>Envoyer</Button>
+              </form>
+            </Card>
+          ))
+        }
       </div>
       <Modal dismissible show={openModal} onClose={() => setOpenModal(false)}>
         <Modal.Header>Créer une carte mémoire</Modal.Header>
@@ -89,7 +131,7 @@ function App() {
           </form>
         </Modal.Body>
         <Modal.Footer>
-          <Button type="submit" color='purple' onClick={handleSubmit}>Créer</Button>
+          <Button type="submit" onClick={handleSubmit}>Créer</Button>
           <Button color="gray" onClick={() => setOpenModal(false)}>Annuler</Button>
         </Modal.Footer>
       </Modal>
