bc682ec7ec43fdfe09141eebff16a182f98de1f2
= <=>
2024-02-20 16:16:36 +0100

    feat(config): add cors settings

--- server/app.js
+++ server/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 4000;
 const cardRoute = require('./src/interfaces/routes/cardRoute');
 // const learningRoute = require('./router/learningRoute')
+app.use(cors({ 
+  origin: '*',
+  methods:['GET', 'POST', 'PUT', 'DELETE'] 
+}));
+
 app.use(express.json());
 
+app.use(bodyParser.json());
 
 app.use('/', cardRoute);
 // app.use('/', learningRoute);
